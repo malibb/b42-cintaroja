3f657926d54513b611f75f01e7dab184a1a6ffbf
malidevf <mali@devf.mx>
2020-06-24 21:08:24 -0500

    ejercicios clases

---
+++ 3-Clases/ejercicios.js
@@ -0,0 +1,15 @@
+/**
+ 1.- Haz una clase llamada Persona que siga las siguientes condiciones:
+	 Sus atributos son: nombre, edad, RFC, sexo, peso y altura.
+	 calcularIMC()
+	 esMayorDeEdad()
+	 El constructor pide nombre, edad,sexo,peso y altura
+     Generar el RFC a partir de el nombre, edad y sexo
+     
+2.- Crear una clase Cuenta que tenga los siguientes atributos y métodos:
+	 -Titular y cantidad, estado 
+	 -ingresar(cantidad)
+	 -retirar(cantidad)
+	 Hacer las validaciones previas
+	 Como regla de negocio no debe de tener más de $900 y menos de $10
+ */
