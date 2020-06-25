b05cdd6118f9cb668d2b534e54b0388d8e7ffe5c
malidevf <mali@devf.mx>
2020-06-24 21:07:42 -0500

    clases metodos

--- 3-Clases/index.js
+++ 3-Clases/index.js
@@ -10,7 +10,7 @@ const carro = {
     frena() {
         console.log('iiiiiii');
     },
-    dobla() {
+    doblar() {
         console.log('el carro está doblando');
     }
 }
@@ -20,14 +20,34 @@ const carro = {
 // tener la palabra reservada class
 
 class Carro {
-    constructor(color, velocidad, ruedas, motor, tipoRueda) {
+    constructor(duenio, color, velocidad, ruedas, motor, tipoRueda) {
         // inicializar variables
+        this.duenio = duenio;
         this.color = color;
         this.velocidad = velocidad;
         this.ruedas = ruedas;
         this.motor = motor;
         this.tipoRueda = tipoRueda;
         this.vendido = false;
+    };
+
+    arranca() {
+        // evaluando que this.vendido sea igual con true
+        if(this.vendido) {
+            console.log(`El carro de ${this.duenio} hace run run`);
+        } else {
+            console.log(`El carro de ${this.duenio} no
+            puede hacer run run porque no esta pagado >.<`);
+        }
+        return true;
+    };
+
+    frena() {
+        console.log('iiiiiii');
+    };
+
+    doblar() {
+        console.log('el carro está doblando');
     };
 }
 
@@ -47,17 +67,20 @@ class Rueda {
 
 // Carro.constructor();
 // opcion 1 para crear un carro con tipo de rueda
-const carro1 = new Carro('rojo', 30, 4, 'V4', new Rueda('aluminio', 34));
+const carro1 = new Carro('César','rojo', 30, 4, 'V4', new Rueda('aluminio', 34));
 // opcion 2 para crear un carro con tipo de rueda
 const tipoRueda = new Rueda('alumino', 32);
-const carro2 = new Carro('rojo', 30, 4, 'V4', tipoRueda);
+const carro2 = new Carro('Daniel', 'rojo', 30, 4, 'V4', tipoRueda);
 
 // snake-case => Snake case
 // camelCase => Camel case
 
-console.log(carro);
-console.log(carro1);
+// console.log(carro);
+// console.log(carro1);
 console.log(carro1.tipoRueda.rodada);
 console.log(carro2.tipoRueda.rodada);
 carro2.precio = 200000;
-console.log(carro2);
+carro1.arranca();
+carro2.vendido = true;
+carro2.arranca();
+// console.log(carro1.arranca());
