71acbea3526a14c5c3983d7297b9b416b2d91f4c
malidevf <mali@devf.mx>
2020-06-24 20:27:33 -0500

    kata 1

--- 2-Objetos/ejercicios.js
+++ 2-Objetos/ejercicios.js
@@ -1,1 +1,19 @@
-console.log('Hola!')
+
+function farenheitToCelsius(gradosF) {
+    const resta = gradosF - 32;
+    const fraccion = 5/9;
+    const multiplicacion = resta * fraccion;
+    return multiplicacion;
+}
+let grados = 25;
+
+/* Arrow */
+const farenheitToCelsius2 = grados => (grados - 32) * (5/9) ;
+
+const resultado = farenheitToCelsius(grados);
+const resultado2 = farenheitToCelsius2(grados);
+console.log(resultado);
+console.log(resultado2);
+
+console.log(farenheitToCelsius(89));
+console.log(farenheitToCelsius2(89));
