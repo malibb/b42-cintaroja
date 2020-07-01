3f0eb73ee19ee1471f4b1f0afc0e4282d3e69493
malidevf <mali@devf.mx>
2020-06-30 19:39:46 -0500

    ejercicio resuelto

--- 6.-Peticiones/index.js
+++ 6.-Peticiones/index.js
@@ -6,9 +6,38 @@ function getOneCivilization(error, response, body) {
     // console.log(JSON.parse(body).unique_unit);
     const newBody = JSON.parse(body);
     console.log(newBody.unique_unit);
+    const [url] = newBody.unique_unit;
+    // cierre de paréntesis :v
     console.log(response.statusCode);
+    // request()
+    request(url, getOneUnit);
     // ¿Cómo le haría para traer la siguiente oración por medio de peticiones?
-    console.log('Es la unidad Jaguar Warrior que se describe como Aztec unique unit. Attack bonus vs. other infantry');
+}
+/*
+// recibe 3 los elementos, y su nombre de grupo
+// devolver un arreglo y el numero de elementos en un arreglo [arreglo de Objetos, numero de Elementos]
+function crearArreglo();
+
+const [arreglo, numero] = crearArreglo('Mali','B42 de roja','Al batch de gera con gera');
+const [arregloDeFrutas, noDeFrutas] = crearArreglo('Pera','manzana','melon');
+
+// Destructuración de arreglos.
+    const arreglo = ['helado','natilla','papas'];
+    // forma común
+    const numero1 = arreglo[1];
+    // forma nice
+    const [,n1,] = arreglo;
+    console.log(n1);
+*/
+
+
+
+function getOneUnit(error, response, body) {
+    const newBody = JSON.parse(body);
+    if(response.statusCode === 200) {
+        console.log(`Es la unidad ${newBody.name} que se describe como ${newBody.description}`);
+    }
+    // ¿Cómo le haría para traer la siguiente oración por medio de peticiones?
 }
 
-request('https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/1', getOneCivilization);
+request('https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/4', getOneCivilization);
