0eb9eb9e69906671877fc0056494cc197d8e2d80
malidevf <mali@devf.mx>
2020-06-30 19:09:23 -0500

    peticion civilizaciones

--- 6.-Peticiones/index.js
+++ 6.-Peticiones/index.js
@@ -7,7 +7,6 @@ function getOneCivilization(error, response, body) {
     const newBody = JSON.parse(body);
     console.log(newBody.unique_unit);
     console.log(response.statusCode);
-    
     // ¿Cómo le haría para traer la siguiente oración por medio de peticiones?
     console.log('Es la unidad Jaguar Warrior que se describe como Aztec unique unit. Attack bonus vs. other infantry');
 }
